3d054b7a8c01c9d7745d2f274448ef2c792caeb7
n0nag0n <n0nag0n@sky-9.com>
2024-01-14 00:06:44 -0700

    Tweaked Readme

--- app/config/config_sample.php
+++ app/config/config_sample.php
@@ -3,7 +3,7 @@
 use Tracy\Debugger;
 
 // Set the default timezone
-date_default_timezone_set('America/New_York');
+date_default_timezone_set('UTC');
 
 // Set the error reporting level
 error_reporting(E_ALL);
@@ -28,7 +28,7 @@ $app->path(__DIR__ . $ds . '..' . $ds . '..');
 $app->set('flight.base_url', '/'); // if this is in a subdirectory, you'll need to change this
 $app->set('flight.case_sensitive', false); // if you want case sensitive routes, set this to true
 $app->set('flight.log_errors', true); // if you want to log errors, set this to true
-$app->set('flight.handle_errors', true); // if you want flight to handle errors, set this to true
+$app->set('flight.handle_errors', false); // if you want flight to handle errors, set this to true
 $app->set('flight.views.path', __DIR__ . $ds . '..' . $ds . 'views'); // set the path to your view/template/ui files
 $app->set('flight.views.extension', '.php'); // set the file extension for your view/template/ui files
 $app->set('flight.content_length', true); // if flight should send a content length header
@@ -59,16 +59,8 @@ if (Debugger::$showBar) {
  * 
  * What you store here is totally up to you.
  */
+
+define('ENVIRONMENT', 'development');
 return [
-	'database' => [
-		'host' => 'localhost',
-		'dbname' => 'dbname',
-		'user' => 'user',
-		'password' => 'password'
-	],
-	'google_oauth' => [
-		'client_id' => 'client_id',
-		'client_secret' => 'client_secret',
-		'redirect_uri' => 'redirect_uri'
-	],
+
 ];
