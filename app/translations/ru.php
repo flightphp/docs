43fad4c69ff4722738798589972404755fa78b74
n0nag0n <n0nag0n@sky-9.com>
2024-02-05 21:59:12 -0700

    forgot a couple files.

---
+++ app/translations/ru.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'subtitle' => 'Расширяемый микро-фреймворк для PHP',
+	'search_the_docs' => 'Поиск в документации',
+	'about' => 'О нас',
+	'install' => 'Установка',
+	'learn' => 'Учебник',
+	'examples' => 'Примеры',
+	'awesome_plugins' => 'Потрясающие плагины',
+	'code' => 'Код',
+	'light' => 'Светлый',
+	'dark' => 'Темный',
+	'made_with_love' => 'Сделано с <b style="color:red;">&lt;3</b> с использованием Flight. Авторское право © Mike Cao/Команда FlightPHP',
+	'license' => 'Лицензия',
+	'missing_documentation' => 'Отсутствует документация? <a href="https://github.com/flightphp/docs" target="_blank">Форкните и помогите улучшить!</a>',
+];
