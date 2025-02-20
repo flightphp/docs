338f67b947b01f6776e09cf5e02af74c635ce1b9
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-19 21:44:44 -0400

    chore(rector): php82 set

--- rector.php
+++ rector.php
@@ -9,8 +9,7 @@ return RectorConfig::configure()
         __DIR__ . '/app',
         __DIR__ . '/public',
     ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
+    ->withPhpSets(php82: true)
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
