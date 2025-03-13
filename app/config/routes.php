d018b106e2e11f97605857e3672f4eb462173b4c
fadrian06 <franyeradriansanchez@gmail.com>
2025-03-13 02:28:03 -0400

    refactor(routes): simplified routes registration, improve code readability and simplified route callbacks

--- app/config/routes.php
+++ app/config/routes.php
@@ -15,75 +15,147 @@ Flight::group('', static function (): void {
      */
     // This processes github webhooks
     Flight::route(
-        'POST /update-stuff',
-        [DocsController::class, 'updateStuffPost'],
-        false,
-        'update_stuff'
+        pattern: 'POST /update-stuff',
+        callback: [DocsController::class, 'updateStuffPost'],
+        alias: 'update_stuff'
     );
 
     /*
      * Redirects
      */
-    // if theres no language or version in the url, redirect and default to en and v3
+    // if theres no language or version in the url, redirect and default to en
+    // and v3
     Flight::route('/', static function (): void {
         // pull out the default language by the accept header
         $language = Translator::getLanguageFromRequest();
-        Flight::redirect('/' . $language . '/v3/');
+
+        Flight::redirect("/$language/v3/");
     });
 
     // If the route only defines a language (ex: /en) redirect with a version
-    Flight::route('/@language:[a-z0-9]{2}', static function (string $language): void {
-        // if there's a number in it, it's actually probably the version so we'll need to pull the language out and consider this a version
-        if (preg_match('/\d/', $language) === 1) {
-            $version = $language;
-            $language = Translator::getLanguageFromRequest();
-            Flight::redirect("/en/$language/");
-        } else {
-            $version = 'v3';
+    Flight::route(
+        '/@language:[a-z0-9]{2}',
+        static function (string $language): void {
+            // if there's a number in it, it's actually probably the version so
+            // we'll need to pull the language out and consider this a version
+            $version = preg_match('/\d/', $language) ? $language : 'v3';
+
+            if (preg_match('/\d/', $language)) {
+                $language = Translator::getLanguageFromRequest();
+                Flight::redirect("/en/$language/");
+            }
+
+            Flight::redirect("/$language/$version/");
         }
-        Flight::redirect("/$language/$version/");
-    });
+    );
 
     // Pick up old routes that didn't use to have a language and version header
-    Flight::route('/@section:[\w\-]{3,}(/@sub_section:[\w\-]{3,})', function (string $section, ?string $sub_section = ''): void {
-        $language = Translator::getLanguageFromRequest();
-        Flight::redirect("/{$language}/v3/$section/$sub_section/");
-    });
+    Flight::route(
+        '/@section:[\w\-]{3,}(/@sub_section:[\w\-]{3,})',
+        static function (string $section, ?string $sub_section = ''): void {
+            $language = Translator::getLanguageFromRequest();
+
+            Flight::redirect("/$language/v3/$section/$sub_section/");
+        }
+    );
 
     /*
      * Core routes
      */
-    Flight::group('/@language:[a-z]{2}/@version:[a-z0-9]{2}', static function (): void {
-        Flight::route('GET /', [DocsController::class, 'aboutGet'], false, 'about');
-        Flight::route('GET /single-page', [DocsController::class, 'singlePageGet'], false, 'single_page');
-        Flight::route('GET /about', [DocsController::class, 'aboutGet']);
-        Flight::route('GET /install', [DocsController::class, 'installGet'], false, 'install');
+    Flight::group(
+        '/@language:[a-z]{2}/@version:[a-z0-9]{2}',
+        static function (): void {
+            Flight::route(
+                pattern: 'GET /',
+                callback: [DocsController::class, 'aboutGet'],
+                alias: 'about'
+            );
 
-        // Unique URL workaround because this is the only 'single page' with a scrollspy for the time being.
-        Flight::route('GET /install/install', static function (): void {
-            Flight::redirect(Flight::getUrl('install'));
-        });
+            Flight::route(
+                pattern: 'GET /single-page',
+                callback: [DocsController::class, 'singlePageGet'],
+                alias: 'single_page'
+            );
 
-        Flight::route('GET /license', [DocsController::class, 'licenseGet'], false, 'license');
-        Flight::route('GET /examples', [DocsController::class, 'examplesGet'], false, 'examples');
-        Flight::route('GET /media', [DocsController::class, 'mediaGet'], false, 'media');
-        Flight::route('GET /search', [DocsController::class, 'searchGet'], false, 'search');
+            Flight::route('GET /about', [DocsController::class, 'aboutGet']);
 
-        Flight::group('/learn', static function (): void {
-            Flight::route('GET ', [DocsController::class, 'learnGet'], false, 'learn');
-            Flight::route('GET /@section_name', [DocsController::class, 'learnSectionsGet']);
-        });
+            Flight::route(
+                pattern: 'GET /install',
+                callback: [DocsController::class, 'installGet'],
+                alias: 'install'
+            );
 
-        Flight::group('/guides', static function (): void {
-            Flight::route('GET ', [DocsController::class, 'guidesGet'], false, 'guides');
-            Flight::route('GET /@section_name', [DocsController::class, 'guidesSectionsGet']);
-        });
+            // Unique URL workaround because this is the only 'single page'
+            // with a scrollspy for the time being.
+            Flight::route('GET /install/install', static function (): void {
+                Flight::redirect(Flight::getUrl('install'));
+            });
 
-        Flight::group('/awesome-plugins', static function (): void {
-            Flight::route('GET ', [DocsController::class, 'awesomePluginsGet'], false, 'awesome_plugins');
-            Flight::route('GET /@plugin_name', [DocsController::class, 'pluginGet'], false, 'plugin');
-        });
-    });
+            Flight::route(
+                pattern: 'GET /license',
+                callback: [DocsController::class, 'licenseGet'],
+                alias: 'license'
+            );
+
+            Flight::route(
+                pattern: 'GET /examples',
+                callback: [DocsController::class, 'examplesGet'],
+                alias: 'examples'
+            );
+
+            Flight::route(
+                pattern: 'GET /media',
+                callback: [DocsController::class, 'mediaGet'],
+                alias: 'media'
+            );
+
+            Flight::route(
+                pattern: 'GET /search',
+                callback: [DocsController::class, 'searchGet'],
+                alias: 'search'
+            );
+
+            Flight::group('/learn', static function (): void {
+                Flight::route(
+                    pattern: 'GET ',
+                    callback: [DocsController::class, 'learnGet'],
+                    alias: 'learn'
+                );
+
+                Flight::route(
+                    'GET /@section_name',
+                    [DocsController::class, 'learnSectionsGet']
+                );
+            });
+
+            Flight::group('/guides', static function (): void {
+                Flight::route(
+                    pattern: 'GET /',
+                    callback: [DocsController::class, 'guidesGet'],
+                    alias: 'guides'
+                );
+
+                Flight::route(
+                    'GET /@section_name',
+                    [DocsController::class, 'guidesSectionsGet']
+                );
+            });
+
+            Flight::group('/awesome-plugins', static function (): void {
+                Flight::route(
+                    pattern: 'GET /',
+                    callback: [DocsController::class, 'awesomePluginsGet'],
+                    alias: 'awesome_plugins'
+                );
+
+                Flight::route(
+                    pattern: 'GET /@plugin_name',
+                    callback: [DocsController::class, 'pluginGet'],
+                    alias: 'plugin'
+                );
+            });
+        }
+    );
 }, [HeaderSecurityMiddleware::class]);
 
 /*
@@ -95,7 +167,7 @@ Flight::map('notFound', static function (): void {
 
     // pull the version out of the URL
     $url = Flight::request()->url;
-    $version = preg_match('~/(v\d)/~', $url, $matches) === 1 ? $matches[1] : 'v3';
+    $version = preg_match('~/(v\d)/~', $url, $matches) ? $matches[1] : 'v3';
 
     (new Container)->get(DocsLogic::class)->renderPage('not_found.latte', [
         'title' => '404 Not Found',
@@ -103,6 +175,4 @@ Flight::map('notFound', static function (): void {
     ]);
 
     Flight::response()->send();
-
-    exit;
 });
