32db61a6dc7f3b0886897ea2d2df00209e7c6bb9
fadrian06 <franyeradriansanchez@gmail.com>
2025-03-13 02:13:36 -0400

    refactor(config): removed duplicated and deprecated configurations

--- app/config/config_sample.php
+++ app/config/config_sample.php
@@ -16,13 +16,8 @@ mb_internal_encoding('UTF-8');
 // Set the default locale
 setlocale(LC_ALL, 'en_US.UTF-8');
 
-Flight::set('flight.base_url', '/'); // if this is in a subdirectory, you'll need to change this
-Flight::set('flight.case_sensitive', false); // if you want case sensitive routes, set this to true
-Flight::set('flight.log_errors', true); // if you want to log errors, set this to true
 Flight::set('flight.handle_errors', false); // if you want flight to handle errors, set this to true
 Flight::set('flight.views.path', __DIR__ . '/../views'); // set the path to your view/template/ui files
-Flight::set('flight.views.extension', '.php'); // set the file extension for your view/template/ui files
-Flight::set('flight.content_length', true); // if flight should send a content length header
 
 /*
  * Get Tracy up and running
@@ -33,14 +28,15 @@ Flight::set('flight.content_length', true); // if flight should send a content l
  * https://tracy.nette.org/
  */
 Debugger::enable(); // auto tries to figure out your environment
-// Debugger::enable(Debugger::DEVELOPMENT) // sometimes you have to be explicit (also Debugger::PRODUCTION)
+// Debugger::enable(Debugger::Development); // sometimes you have to be explicit (also Debugger::Production)
 // Debugger::enable('23.75.345.200'); // you can also provide an array of IP addresses
 Debugger::$logDirectory = __DIR__ . '/../log';
 Debugger::$strictMode = true; // display all errors
 // Debugger::$strictMode = E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED; // all errors except deprecated notices
 
+// if Debugger bar is visible, then content-length can not be set by Flight
 if (Debugger::$showBar) {
-    Flight::set('flight.content_length', false); // if Debugger bar is visible, then content-length can not be set by Flight
+    Flight::set('flight.content_length', false);
     (new Container)->get(TracyExtensionLoader::class);
 }
 
