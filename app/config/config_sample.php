3a4e624ba910457b04615e72bcf3bf46d381b984
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-11 19:28:08 -0400

    remove $app usage in config_sample.php

--- app/config/config_sample.php
+++ app/config/config_sample.php
@@ -1,5 +1,6 @@
 <?php
 
+use flight\Container;
 use flight\debug\tracy\TracyExtensionLoader;
 use Tracy\Debugger;
 
@@ -18,16 +19,14 @@ setlocale(LC_ALL, 'en_US.UTF-8');
 /*
  * Set some flight variables
  */
-$app ??= Flight::app();
-
-$app->path(__DIR__ . '/../..');
-$app->set('flight.base_url', '/'); // if this is in a subdirectory, you'll need to change this
-$app->set('flight.case_sensitive', false); // if you want case sensitive routes, set this to true
-$app->set('flight.log_errors', true); // if you want to log errors, set this to true
-$app->set('flight.handle_errors', false); // if you want flight to handle errors, set this to true
-$app->set('flight.views.path', __DIR__ . '/../views'); // set the path to your view/template/ui files
-$app->set('flight.views.extension', '.php'); // set the file extension for your view/template/ui files
-$app->set('flight.content_length', true); // if flight should send a content length header
+Flight::path(__DIR__ . '/../..');
+Flight::set('flight.base_url', '/'); // if this is in a subdirectory, you'll need to change this
+Flight::set('flight.case_sensitive', false); // if you want case sensitive routes, set this to true
+Flight::set('flight.log_errors', true); // if you want to log errors, set this to true
+Flight::set('flight.handle_errors', false); // if you want flight to handle errors, set this to true
+Flight::set('flight.views.path', __DIR__ . '/../views'); // set the path to your view/template/ui files
+Flight::set('flight.views.extension', '.php'); // set the file extension for your view/template/ui files
+Flight::set('flight.content_length', true); // if flight should send a content length header
 
 /*
  * Get Tracy up and running
@@ -46,8 +45,8 @@ Debugger::$strictMode = true; // display all errors
 
 if (Debugger::$showBar) {
     // if Debugger bar is visible, then content-length can not be set by Flight
-    $app->set('flight.content_length', false);
-    new TracyExtensionLoader($app);
+    Flight::set('flight.content_length', false);
+    Container::getInstance()->get(TracyExtensionLoader::class);
 }
 
 /*
