30d70381a96e390b469f279812ebd14b8e988efc
fadrian06 <franyeradriansanchez@gmail.com>
2025-09-11 15:20:16 -0400

    ignore StringClassNameToClassConstantRector rector rule

--- rector.php
+++ rector.php
@@ -3,10 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/app',
         __DIR__ . '/public',
     ])
-    ->withPhpSets(php82: true);
+    ->withPhpSets(php82: true)
+    ->withSkip([
+        StringClassNameToClassConstantRector::class,
+        __DIR__ . '/app/cache',
+    ])
+;
